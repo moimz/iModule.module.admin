79ab3d3c1c1a9b5ea30031e16fe0c0a883f1f2af
arzzcom <arzz@arzz.com>
2018-06-21 16:33:34 +0900

    변수직접접근 수정

--- process/@getModule.php
+++ process/@getModule.php
@@ -8,7 +8,7 @@
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
  * @version 3.0.0
- * @modified 2018. 3. 18.
+ * @modified 2018. 6. 21.
  */
 if (defined('__IM__') == false) exit;
 
@@ -17,19 +17,19 @@ $module = Request('target');
 /**
  * 모듈 package.json 정보를 가져온다.
  */
-$package = $this->Module->getPackage($module);
+$package = $this->getModule()->getPackage($module);
 if ($package == null) {
 	$results->success = false;
 	$results->message = $this->getErrorText('NOT_FOUND');
 } else {
 	$data = new stdClass();
 	$data->icon = $package->icon;
-	$data->title = $this->Module->getTitle($module);
+	$data->title = $this->getModule()->getTitle($module);
 	$data->version = $package->version;
 	$data->author = $package->author->name.' (<a href="mailto:'.$package->author->email.'">'.$package->author->email.'</a>)';
 	$data->homepage = '<a href="mailto:'.$package->homepage.'" target="_blank">'.$package->homepage.'</a>';
 	$data->language = $package->language;
-	$data->description = $this->Module->getDescription($module);
+	$data->description = $this->getModule()->getDescription($module);
 	
 	$data->context = $package->context;
 	$data->global = $package->global;
@@ -44,18 +44,18 @@ if ($package == null) {
 				'checked'=>version_compare($version,__IM_VERSION__,'>=') == true
 			);
 		} else {
-			$name = $this->Module->getTitle($dependency);
+			$name = $this->getModule()->getTitle($dependency);
 			
 			$data->dependencies[] = array(
 				'name'=>($name !== null ? $name : $dependency).' v'.$version,
-				'checked'=>$this->Module->isInstalled($dependency) === true && version_compare($version,$package->version,'>=') == true
+				'checked'=>$this->getModule()->isInstalled($dependency) === true && version_compare($version,$package->version,'>=') == true
 			);
 		}
 	}
 	
-	$data->isInstalled = $this->Module->isInstalled($module);
-	$data->isLatest = $data->isInstalled == true && $this->Module->getInstalled($module)->hash == $this->Module->getHash($module);
-	$data->isConfigPanel = $this->Module->isConfigPanel($module);
+	$data->isInstalled = $this->getModule()->isInstalled($module);
+	$data->isLatest = $data->isInstalled == true && $this->getModule()->getInstalled($module)->hash == $this->getModule()->getHash($module);
+	$data->isConfigPanel = $this->getModule()->isConfigPanel($module);
 	
 	$results->success = true;
 	$results->data = $data;
