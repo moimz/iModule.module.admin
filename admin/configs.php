cc39d901d2c3f99190b7be2d8d87402cba464d13
arzzcom <arzz@arzz.com>
2019-12-13 16:52:06 +0900

    데이터베이스관리 접근권한 설정추가

--- admin/configs.php
+++ admin/configs.php
@@ -8,7 +8,7 @@
  * @author Arzz (arzz@arzz.com)
  * @license GPLv3
  * @version 3.1.0
- * @modified 2018. 3. 18.
+ * @modified 2019. 12. 11.
  */
 if (defined('__IM__') == false) exit;
 ?>
@@ -16,7 +16,7 @@ if (defined('__IM__') == false) exit;
 new Ext.form.Panel({
 	id:"ModuleConfigForm",
 	border:false,
-	bodyPadding:10,
+	bodyPadding:"10 10 5 10",
 	width:700,
 	fieldDefaults:{labelAlign:"right",labelWidth:100,anchor:"100%",allowBlank:true},
 	items:[
@@ -46,6 +46,28 @@ new Ext.form.Panel({
 					name:"emergency_code",
 					disabled:true,
 					afterBodyEl:'<div class="x-form-help">접근허용IP가 아닌 곳에서 일시적으로 관리자모드에 접근하고자 할때 사용할 코드를 입력하여 주십시오.<br>비상코드를 이용해 관리자모드에 접근하실려면 '+location.protocol+'//'+location.host+ENV.DIR+'/admin/[입력한비상코드] 주소를 통해 접근가능합니다.</div>'
+				})
+			]
+		}),
+		new Ext.form.FieldSet({
+			title:"데이터베이스 보안설정",
+			items:[
+				new Ext.form.Checkbox({
+					fieldLabel:"IP제한 활성화",
+					name:"enable_security_database_mode",
+					boxLabel:"관리자모드의 데이터베이스관리를 특정 IP에서만 접근할 수 있도록 설정합니다.",
+					uncheckedValue:"off",
+					listeners:{
+						change:function(form,checked) {
+							form.getForm().findField("allow_database_ip").setDisabled(!checked);
+						}
+					}
+				}),
+				new Ext.form.TextArea({
+					fieldLabel:"접근허용IP",
+					name:"allow_database_ip",
+					disabled:true,
+					afterBodyEl:'<div class="x-form-help">관리자모드 데이터베이스관리에 접근을 허용할 아이피를 줄바꿈으로 구분하여 입력하여 주십시오.<br>특정IP 또는 IP대역(123.123.123.* 또는 123.*.*.* 등)으로 입력하실 수 있습니다.</div>'
 				})
 			]
 		})
